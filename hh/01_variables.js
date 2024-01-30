b21cca437cd138fc01997a536afd4925cca062a5
kanchanj123 <itkanchan1@gmail.com>
2024-01-30 18:23:05 +0000

    varialbles

---
+++ hh/01_variables.js
@@ -0,0 +1,8 @@
+const accountid=12341234
+let accountemail="anshu@gmail.com"
+var accountpassword="msi@123"
+accountcity="Jaipur"
+let accountstate;
+//accountid=1234567890//
+console.log(accountid)
+console.table([accountid,accountemail,accountpassword,accountcity,accountstate]);
