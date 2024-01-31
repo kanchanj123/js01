c693864412c55dabfa6c5bfadedf42004a92d2cc
kanchanj123 <itkanchan1@gmail.com>
2024-01-31 17:31:30 +0000

    datatype conversion operation

---
+++ hh/03_conversionoperation.js
@@ -0,0 +1,9 @@
+let score = "ggg"
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueOfNum = Number(score);
+//console.log(valueOfNum);
+//console.log(typeof valueOfNum);
+
+let valueOfboolean = Boolean(score);
+console.log(valueOfboolean);
