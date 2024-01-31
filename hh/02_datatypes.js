b544b3a0643e00d884175196e279f3c12245d932
kanchanj123 <itkanchan1@gmail.com>
2024-01-31 17:32:08 +0000

    datatypes

---
+++ hh/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict" // treate all js code as newer virson
+// alert(3+5)// we are using  nodejs not browser
+
+console.log(3+3)
+console.log("anshu");
+const accountid=12345678
+console.log(typeof accountid);
